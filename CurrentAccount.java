c00f2e52e6e4b03b201394133ee477527347a1a8
Spoorthi-K-S <57183859+Spoorthi-K-S@users.noreply.github.com>
2021-08-20 10:00:15 +0530

    Update CurrentAccount.java

--- CurrentAccount.java
+++ CurrentAccount.java
@@ -4,7 +4,7 @@ public class CurrentAccount implements BankAccount {
 
 	@Override
 	public void checkBankBalance() {
-		// TODO Auto-generated method stub
+		
 
 		System.out.println("this is from current account to check bank balance");
 	}
@@ -13,7 +13,7 @@ public class CurrentAccount implements BankAccount {
 
 	@Override
 	public void validateUser() {
-		// TODO Auto-generated method stub
+		
 		System.out.println("this is from current account to valid user");
 		
 	}
