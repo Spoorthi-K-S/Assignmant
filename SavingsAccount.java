a54642e2a607de63d7a6c584d4f08f1bf8613901
Spoorthi-K-S <57183859+Spoorthi-K-S@users.noreply.github.com>
2021-08-20 09:57:09 +0530

    Update SavingsAccount.java

--- SavingsAccount.java
+++ SavingsAccount.java
@@ -4,7 +4,7 @@ public class SavingsAccount implements BankAccount{
 
 	@Override
 	public void checkBankBalance() {
-		// TODO Auto-generated method stub
+		
 
 		System.out.println("this is from savings account to check bank balance");
 		
